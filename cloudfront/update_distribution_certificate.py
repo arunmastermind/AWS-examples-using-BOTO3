a7f648ba40765371daf6f477edd483dbcb44ca8d
arunmastermind <arunmastermind.sci@gmail.com>
2019-12-11 14:55:07 +0530

    Amazon CloudFront - Amazon AWS

--- cloudfront/update_distribution_certificate.py
+++ cloudfront/update_distribution_certificate.py
@@ -1,17 +1,3 @@
-# Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License"). You
-# may not use this file except in compliance with the License. A copy of
-# the License is located at
-#
-# http://aws.amazon.com/apache2.0/
-#
-# or in the "license" file accompanying this file. This file is
-# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
-# ANY KIND, either express or implied. See the License for the specific
-# language governing permissions and limitations under the License. 
-# snippet-start:[cloudfront.python.update_distribution_certificate.complete]
-
 import boto3
 import sys
 
@@ -64,18 +50,3 @@ distribution_config['ViewerCertificate']['Certificate']=new_cert_arn
 
 cf.update_distribution(DistributionConfig=distribution_config, \
 Id=distribution_id,IfMatch=distribution_etag)
- 
- 
-# snippet-end:[cloudfront.python.update_distribution_certificate.complete]
-# snippet-comment:[These are tags for the AWS doc team's sample catalog. Do not remove.]
-# snippet-sourcedescription:[update_distribution_certificate.py demonstrates how to update multiple Amazon CloudFront distributions.]
-# snippet-keyword:[Python]
-# snippet-sourcesyntax:[python]
-# snippet-sourcesyntax:[python]
-# snippet-keyword:[AWS SDK for Python (Boto3)]
-# snippet-keyword:[Code Sample]
-# snippet-keyword:[Amazon CloudFront]
-# snippet-service:[cloudfront]
-# snippet-sourcetype:[full-example]
-# snippet-sourcedate:[2018-12-26]
-# snippet-sourceauthor:[walkerk1980]
